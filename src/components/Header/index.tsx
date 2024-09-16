986ea99730c1d5e272cff18d2db304283676bac0
PedroLucasAraujo <pedrolucas.araujo@agenciam3.com>
2024-09-16 19:37:30 -0300

    feat: create accessible modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,25 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logo from "../../assets/logo.svg";
+import * as Dialog from "@radix-ui/react-dialog";
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logo} alt="DT Money" />
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
