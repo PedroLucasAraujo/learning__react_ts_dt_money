e50e454d9bedeb10695e754d7a17c99375c3b8f2
PedroLucasAraujo <pedrolucas.araujo@agenciam3.com>
2024-09-17 19:10:26 -0300

    feat: create controlled transaction type input

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,12 +1,39 @@
 import { MagnifyingGlass } from "phosphor-react";
 import { SearchFormContainer } from "./styles";
+import { useForm } from "react-hook-form";
 
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const searchFormSchema = z.object({
+  query: z.string(),
+});
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>;
 export function SearchForm() {
-  return (
-    <SearchFormContainer>
-      <input placeholder="Busque por transações" type="text" />
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  });
 
-      <button type="submit">
+  async function handleSearchTransactions(data: SearchFormInputs) {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    console.log(data);
+  }
+
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        placeholder="Busque por transações"
+        type="text"
+        {...register("query")}
+      />
+
+      <button type="submit" disabled={isSubmitting}>
         <MagnifyingGlass />
         Buscar
       </button>
