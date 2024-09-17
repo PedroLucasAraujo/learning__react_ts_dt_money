8de80241984200e3111499f405a443eab32bd2c7
PedroLucasAraujo <pedrolucas.araujo@agenciam3.com>
2024-09-17 19:25:55 -0300

    fix: refactoring transactions fetch to axios request instead of fetch api

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,5 @@
 import React, { createContext, useEffect, useState } from "react";
+import { api } from "../lib/axios";
 
 interface Transaction {
   id: string;
@@ -23,15 +24,13 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
   async function fetchTransactions(query?: string) {
-    const url = new URL("http://localhost:3000/transactions");
+    const response = await api.get("/transactions", {
+      params: {
+        q: query,
+      },
+    });
 
-    if (query) {
-      url.searchParams.append("q", query);
-    }
-    const response = await fetch(url);
-    const data: any = response.json();
-    console.log(data);
-    setTransactions(data);
+    setTransactions(response.data);
   }
 
   useEffect(() => {
