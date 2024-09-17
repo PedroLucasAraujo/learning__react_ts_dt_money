93bc82b57eec553a1a51604332596d21d3b604fc
PedroLucasAraujo <pedrolucas.araujo@agenciam3.com>
2024-09-17 19:21:33 -0300

    feat: creating transactions fetch with queries

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -10,6 +10,7 @@ interface Transaction {
 }
 interface TransactionsContextType {
   transactions: Transaction[];
+  fetchTransactions: (query?: string) => Promise<void>;
 }
 
 interface TransactionProviderProps {
@@ -21,19 +22,24 @@ export const TransactionsContext = createContext({} as TransactionsContextType);
 export function TransactionsProvider({ children }: TransactionProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-  async function loadTransactions() {
-    const response = await fetch("http://localhost:3000/transactions");
+  async function fetchTransactions(query?: string) {
+    const url = new URL("http://localhost:3000/transactions");
+
+    if (query) {
+      url.searchParams.append("q", query);
+    }
+    const response = await fetch(url);
     const data: any = response.json();
     console.log(data);
     setTransactions(data);
   }
 
   useEffect(() => {
-    loadTransactions();
+    fetchTransactions();
   }, []);
 
   return (
-    <TransactionsContext.Provider value={{ transactions }}>
+    <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>
       {children}
     </TransactionsContext.Provider>
   );
